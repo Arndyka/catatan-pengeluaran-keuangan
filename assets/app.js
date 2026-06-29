9c8f59665bce439a71dbe2e48acf2e1a70bee3cd
Arndyka <arnolduzandy123@gmail.com>
2026-06-29 14:15:19 +0700

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -5,12 +5,14 @@
 */
 
 const firebaseConfig = {
-  apiKey: "ISI_API_KEY_KAMU",
-  authDomain: "ISI_PROJECT_ID.firebaseapp.com",
-  projectId: "ISI_PROJECT_ID",
-  storageBucket: "ISI_PROJECT_ID.appspot.com",
-  messagingSenderId: "ISI_MESSAGING_SENDER_ID",
-  appId: "ISI_APP_ID"
+  apiKey: "AIzaSyBTI3nR4CA5HdVGeP3zg7YibS2kfEvcCNc",
+  authDomain: "catatan-pengeluaran-keua-19af4.firebaseapp.com",
+  databaseURL: "https://catatan-pengeluaran-keua-19af4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "catatan-pengeluaran-keua-19af4",
+  storageBucket: "catatan-pengeluaran-keua-19af4.firebasestorage.app",
+  messagingSenderId: "582629555317",
+  appId: "1:582629555317:web:4e8a943c221f53f96bcc3c",
+  measurementId: "G-GDWT3NWNMH"
 };
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
